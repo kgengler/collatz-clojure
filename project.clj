f138a5c78e4d6029b5af17ff47ac6f26c9edf8a1
Kyle Gengler <kyle.gengler@gmail.com>
2016-09-30 23:57:57 -0500

    update project information

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject collatz-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject collatz-clojure "1.0.0"
+  :description "Takes a number and calculates the number of steps to reach 1 using steps outlined in the Collatz Conjecture"
+  :url "https://github.com/kgengler/collatz-clojure"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot collatz-clojure.core
   :target-path "target/%s"
